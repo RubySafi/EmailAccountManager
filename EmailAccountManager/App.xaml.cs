bcf77f3b9cd185f410eca1030cd973d7d1196ddc
Ruby <onoda319mirai@zpost.plala.or.jp>
2025-05-05 11:10:43 +0900

    minor update [multiple instance check]

--- EmailAccountManager/App.xaml.cs
+++ EmailAccountManager/App.xaml.cs
@@ -9,6 +9,32 @@ namespace EmailAccountManager
     /// </summary>
     public partial class App : Application
     {
+        private Mutex _mutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string mutexName = "EmailAccountManager_v1_RubySafi";
+
+            bool isNewInstance;
+            _mutex = new Mutex(true, mutexName, out isNewInstance);
+
+            if (!isNewInstance)
+            {
+                MessageBox.Show("This application is already running.",
+                    "Warning: Multiple instances detected", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _mutex?.ReleaseMutex();
+            _mutex?.Dispose();
+            base.OnExit(e);
+        }
     }
 
 }
--- EmailAccountManager/AsmUtility.cs
+++ EmailAccountManager/AsmUtility.cs
@@ -11,7 +11,7 @@ namespace EmailAccountManager
     public static class AsmUtility
     {
         public const int MeasureVersion = 1;
-        public const int MinorVersion = 6;
+        public const int MinorVersion = 7;
         public static string GetAssemblyDirectoryName()
         {
             string location = Assembly.GetEntryAssembly().Location;
